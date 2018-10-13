52fb070a1fcdb085bb36189f412350f3686eef5e
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-10-13 18:11:08 +0200

    Open the current directory as a git repository
    PROGRESS!!!!11oneoneeleven

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-fn main() {
+extern crate git2;
+
+use git2::Repository;
+use std::env;
+
+fn main() -> Result<(), Box<std::error::Error>> {
     let curses = vec!["arse", "ass", "asshole", "bitch", "balls", "bollock", "bugger", "cock", "cunt", "dick", "fuck", "goddamn", "damn", "shit", "crap", "piss", "shit", "shite"];
+    let path = env::current_dir()?;
+    let repo = Repository::open(path)?;
     println!("{:?}", curses);
+    println!("{:?}", repo.workdir());
+    Ok(())
 }
