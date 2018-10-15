585f7a1674b9e53cbf53f80d681ea199811ae73e
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-10-15 16:36:58 +0200

    Fix wrong ordering on counting commits

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ impl fmt::Display for Author {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "{}: ({}/{}) commits/naughty commits\n{:#?}",
+            "{}: ({}/{}) naughty commits/commits\n{:#?}",
             self.name, self.total_curses, self.total_commits, self.curses
         )
     }
