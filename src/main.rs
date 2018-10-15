395a6171d8b392e897aaa96ad48461d157968fa1
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-10-15 03:58:14 +0200

    Walk the commits, find the authors, see if they've been naughty
    IT WORKS! HOLY SHIT.

--- src/main.rs
+++ src/main.rs
@@ -54,22 +54,31 @@ fn main() -> Result<(), Box<Error>> {
     let path = env::current_dir()?;
     let repo = Repository::open(path)?;
     let mut revwalk = repo.revwalk()?;
-    let mut occurrences: HashMap<&str, usize> = HashMap::new();
     let mut commits: Vec<Commit> = Vec::new();
-    for curse in &curses {
-        occurrences.entry(curse).or_insert(0);
-    }
     revwalk.push_head()?;
     for commit in revwalk {
         let commit = repo.find_commit(commit?)?;
         commits.push(commit);
     }
     let mut authors: Vec<Author> = find_authors(&commits);
-    println!("{:?}", repo.workdir());
-    filter_occurrences(&mut occurrences);
-    println!("{:#?}", occurrences);
-    println!("{:#?}", commits);
-    print!("{:#?}", authors);
+    for commit in &commits {
+        let text = commit.message_raw().unwrap().to_lowercase().to_string();
+        let author = commit.author().name().unwrap().to_string();
+        let index = authors.iter().position(|i| i.name == author).expect("Could not find author");
+        let mut author = authors.get_mut(index).unwrap();
+        for word in text.split_whitespace() {
+            if naughty_word(word, &curses) {
+                author.update_occurrence(word);
+            }
+        }
+    }
+    for mut author in authors {
+        author.filter_occurrences();
+        if !author.is_not_naughty() {
+            println!("{}", author.name);
+            println!("{:#?}", author.curses);
+        }
+    }
     Ok(())
 }
 
@@ -79,23 +88,15 @@ fn find_authors(commits: &[Commit]) -> Vec<Author> {
     for commit in commits {
         let name = commit.author().name().unwrap().to_string();
         if !names.contains(&name) {
-            res.push(Author::new(name.as_str(), HashMap::new()));
+            res.push(Author::new(name.as_str()));
         }
         names.push(name);
     }
     res
 }
 
-fn update_occurrence<'a>(word: &'a str, map: &mut HashMap<&'a str, usize>) {
-    map.entry(word).and_modify(|i| *i += 1);
-}
-
 fn naughty_word(word: &str, naughty_list: &[&str]) -> bool {
     naughty_list.contains(&word)
-}
-
-fn filter_occurrences(map: &mut HashMap<&str, usize>) {
-    map.retain(|_, val| val > &mut 0);
 }
 
 #[cfg(test)]
@@ -108,42 +109,5 @@ mod test {
         assert!(naughty_word("fuck", &curses));
         assert!(naughty_word("cyberfuckers", &curses));
         assert!(!naughty_word("pretty", &curses));
-    }
-
-    #[test]
-    fn test_update_occurrences() {
-        let curses: Vec<&str> = CURSES.lines().collect();
-        let mut occurrences: HashMap<&str, usize> = HashMap::new();
-        for curse in &curses {
-            occurrences.entry(curse).or_insert(0);
-        }
-        update_occurrence("boobs", &mut occurrences);
-        update_occurrence("crap", &mut occurrences);
-        update_occurrence("boobs", &mut occurrences);
-
-        assert_eq!(2, occurrences.remove("boobs").unwrap());
-        assert_eq!(1, occurrences.remove("crap").unwrap());
-    }
-
-    #[test]
-    fn test_filter_occurrences() {
-        let curses: Vec<&str> = CURSES.lines().collect();
-        let mut occurrences: HashMap<&str, usize> = HashMap::new();
-        let actual: HashMap<&str, usize> =
-            [("shite", 1), ("goddamn", 1), ("shit", 1), ("fucking", 1)]
-                .iter()
-                .cloned()
-                .collect();
-        for curse in &curses {
-            occurrences.entry(curse).or_insert(0);
-        }
-        for word in "this is a fucking shit sentence with no goddamn shite in it".split_whitespace()
-        {
-            if naughty_word(&word, &curses) {
-                update_occurrence(word, &mut occurrences);
-            }
-        }
-        filter_occurrences(&mut occurrences);
-        assert_eq!(actual, occurrences);
     }
 }
