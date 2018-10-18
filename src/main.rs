e1ab587700ba0f45f2da46120ed794e623d11a0b
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-10-18 22:40:01 +0200

    More suggestions from #1
    Add authors to the repo struct, I was silly and thought it was
    reasonable to put them in a HashSet but then I couldn't retrieve them
    because I had nothing to match them against. Using a HashMap where the
    key is what I should've seen... but I didn't.
    
    Add function that lazily adds authors to the repo, a better structure
    of the main loop and a much better way of adding and retrieving authors.

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,7 @@ struct Repo {
     name: String,
     total_commits: usize,
     total_curses: usize,
+    authors: HashMap<String, Author>,
 }
 
 impl fmt::Display for Repo {
@@ -50,13 +51,23 @@ impl fmt::Display for Repo {
 }
 
 impl Repo {
-    fn new(name: &str) -> Self {
-        let name = name.to_string();
+    fn new(name: impl Into<String>) -> Self {
         Repo {
-            name,
+            name: name.into(),
             total_commits: 0,
             total_curses: 0,
+            authors: HashMap::new(),
         }
+    }
+
+    fn author(&mut self, author_name: &str) -> &mut Author {
+        if !self.authors.contains_key(author_name) {
+            self.authors
+                .entry(author_name.into())
+                .or_insert_with(|| Author::new(author_name));
+        }
+
+        self.authors.get_mut(author_name).expect("exists")
     }
 }
 
@@ -79,23 +90,17 @@ impl fmt::Display for Author {
 }
 
 impl Author {
-    fn new(name: &str) -> Self {
-        let name = name.to_string();
-        let curses: HashMap<String, usize> = HashMap::new();
+    fn new(name: impl Into<String>) -> Self {
         Author {
-            name,
-            curses,
+            name: name.into(),
+            curses: HashMap::new(),
             total_commits: 0,
             total_curses: 0,
         }
     }
 
-    fn update_occurrence(&mut self, curse: &str) {
-        if !self.curses.contains_key(curse) {
-            self.curses.insert(curse.to_string(), 1);
-        } else {
-            self.curses.entry(curse.to_string()).and_modify(|i| *i += 1);
-        }
+    fn update_occurrence(&mut self, curse: impl Into<String>) {
+        *self.curses.entry(curse.into()).or_insert(0) += 1;
     }
 
     fn is_naughty(&self) -> bool {
@@ -121,36 +126,32 @@ fn main() -> Result<(), Box<Error>> {
         commits.push(commit);
     }
     let mut repo = Repo::new(path.file_name().unwrap().to_str().unwrap());
-    let mut authors: Vec<Author> = find_authors(&commits);
     for commit in &commits {
-        let author_name = commit
-            .author()
-            .name()
-            .map(|n| n.to_owned())
-            .expect("No author found");
         let commit_message = commit
             .message()
             .map(|msg| msg.to_lowercase())
             .expect("No commit message found");
-        let index = authors
-            .iter()
-            .position(|i| i.name == author_name)
-            .expect("Could not find author");
-        let mut author = &mut authors[index];
-        author.total_commits += 1;
-        repo.total_commits += 1;
-        for word in split_into_clean_words(&commit_message) {
-            if naughty_word(word, &curses) {
-                author.total_curses += 1;
-                repo.total_curses += 1;
-                author.update_occurrence(word);
+        if let Some(author_name) = commit.author().name() {
+            let mut curses_added = 0;
+            {
+                let author = repo.author(author_name);
+                author.total_commits += 1;
+                for word in split_into_clean_words(&commit_message) {
+                    if naughty_word(word, &curses) {
+                        author.total_curses += 1;
+                        curses_added += 1;
+                        author.update_occurrence(word);
+                    }
+                }
             }
+            repo.total_commits += 1;
+            repo.total_curses += curses_added;
         }
     }
     let end = Instant::now();
     println!("{:?}", end.duration_since(start));
     println!("{}", repo);
-    for mut author in authors {
+    for mut author in repo.authors.values() {
         if author.is_naughty() {
             println!("{}", author);
         }
@@ -162,19 +163,6 @@ fn split_into_clean_words(input: &str) -> impl Iterator<Item = &str> {
     input
         .split(|c: char| !char::is_alphabetic(c))
         .filter(|w| !w.is_empty())
-}
-
-fn find_authors(commits: &[Commit]) -> Vec<Author> {
-    let mut names: Vec<String> = Vec::new();
-    let mut res: Vec<Author> = Vec::new();
-    for commit in commits {
-        let name = commit.author().name().unwrap().to_string();
-        if !names.contains(&name) {
-            res.push(Author::new(name.as_str()));
-        }
-        names.push(name);
-    }
-    res
 }
 
 fn naughty_word(word: &str, naughty_list: &HashSet<&str>) -> bool {
